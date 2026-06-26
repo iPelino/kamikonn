b0665e8308c137ec11db77aae77aee19a812f43d
Pelin Mutanguha <pelmut2000@gmail.com>
2026-06-26 10:23:22 +0200

    [Epic 10] Production Infrastructure & Storage (#86)
    * feat(infrastructure): setup production Docker, Nginx, Certbot and DO Spaces (closes #40, #41)
    
    * chore: Update domain to kamikonn.rw

--- backend/kamikonn/settings/production.py
+++ backend/kamikonn/settings/production.py
@@ -31,8 +31,19 @@ SECURE_HSTS_PRELOAD = True
 
 # Static files / Storage
 # We will use DigitalOcean spaces for production
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+AWS_ACCESS_KEY_ID = config("DO_SPACES_ACCESS_KEY_ID", default="")
+AWS_SECRET_ACCESS_KEY = config("DO_SPACES_SECRET_ACCESS_KEY", default="")
+AWS_STORAGE_BUCKET_NAME = config("DO_SPACES_BUCKET_NAME", default="")
+AWS_S3_ENDPOINT_URL = config("DO_SPACES_ENDPOINT_URL", default="")
+AWS_S3_REGION_NAME = config("DO_SPACES_REGION", default="fra1")
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+AWS_LOCATION = "media"  # optional, if media files should be in a subfolder
+AWS_DEFAULT_ACL = "public-read"
+
+STORAGES = {
+    "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
+    "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
+}
 
 # Email
 MAILERS = {
